3aa7085a3504c591e8ebb0f9e0489a14c1a92ee1
web-dev-kush <webdevkush23@gmail.com>
2025-12-14 20:43:00 +0530

    Update CartSlice Component

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -5,9 +5,20 @@ const cartSlice = createSlice({
   initialState: {
     cart: [],
   },
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+  },
 });
 
-export const {} = cartSlice.actions;
+// ✅ EXPORT ACTIONS
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
+// ✅ EXPORT REDUCER
 export default cartSlice.reducer;
